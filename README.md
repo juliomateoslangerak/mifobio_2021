a55dc586d0d3cde8357bc696fb3f43f3c7e8a40a
julio <juiio.mateos-langerak@igh.cnrs.fr>
2021-11-05 10:10:01 +0100

    Added some files and links

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## MiFoBio 2021
 ### A102-Publishing FAIR-ly with OMERO
 
-This repository contains the links and materials for the workshop "Publishing FAIR-ly with OMERO" at [**MiFoBio 2021**](http://imabio-cnrs.fr/mifobio/programme/)'s edition organized by the GDR-IMABIO.
+This repository contains the links and materials for the workshop "Publishing FAIR-ly with OMERO" at [**MiFoBio 2021**](http://imabio-cnrs.fr/mifobio/programme/)'s edition organized by the [**GDR-IMABIO**](http://imabio-cnrs.fr).
 
 The links that will be used during the workshop are the following:
 
-
+- Our OMERO server https://bioimage.france-bioinformatique.fr/demo-mifobio2021
+- Everything about FAIR https://www.go-fair.org
+- Official OMERO guides https://omero-guides.readthedocs.io/en/latest/
---
+++ notebooks/populate_kvp.py
@@ -0,0 +1,97 @@
+manders_HA_Pc = {
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-01-ApoTomeRAWConvert-01.czi': 0.024235392,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-02-ApoTomeRAWConvert-02.czi': 0.020301996,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-03-ApoTomeRAWConvert-03.czi': 0.02308159,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-04-ApoTomeRAWConvert-04.czi': 0.025395133,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-05-ApoTomeRAWConvert-05.czi': 0.030059202,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-06-ApoTomeRAWConvert-06.czi': 0.01733894,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-07-ApoTomeRAWConvert-07.czi': 0,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-08-ApoTomeRAWConvert-08.czi': 0.0066610775,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-09-ApoTomeRAWConvert-09.czi': 0.005341978,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-10-ApoTomeRAWConvert-10.czi': 0.03103469,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-11-ApoTomeRAWConvert-11.czi': 0.0072248704,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-12-ApoTomeRAWConvert-12.czi': 0.023716137,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-01-ApoTomeRAWConvert-13.czi': 0.84051263,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-02-ApoTomeRAWConvert-14.czi': 0.60945165,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-03-ApoTomeRAWConvert-15.czi': 0.6494526,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-04-ApoTomeRAWConvert-16.czi': 0.8830619,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-05-ApoTomeRAWConvert-17.czi': 0.8925218,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-06-ApoTomeRAWConvert-18.czi': 0.7680511,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-07-ApoTomeRAWConvert-19.czi': 0.82021,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-08-ApoTomeRAWConvert-20.czi': 0.7419418,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-09-ApoTomeRAWConvert-21.czi': 0.52930737,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-10-ApoTomeRAWConvert-22.czi': 0.77858347,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-11-ApoTomeRAWConvert-23.czi': 0.8523609,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-12-ApoTomeRAWConvert-24.czi': 0.27138266,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-13-ApoTomeRAWConvert-25.czi': 0.17080036,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-14-ApoTomeRAWConvert-26.czi': 0.39287025,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-01-ApoTomeRAWConvert-15.czi': 0.037871245,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-02-ApoTomeRAWConvert-16.czi': 0.13417037,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-03-ApoTomeRAWConvert-17.czi': 0.034028463,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-04-ApoTomeRAWConvert-18.czi': 0.11444742,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-05-ApoTomeRAWConvert-19.czi': 0.13671942,
+}
+manders_Pc_HA = {
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-01-ApoTomeRAWConvert-01.czi': 0.7507072,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-02-ApoTomeRAWConvert-02.czi': 0.9119749,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-03-ApoTomeRAWConvert-03.czi': 0.40414625,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-04-ApoTomeRAWConvert-04.czi': 0.5952038,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-05-ApoTomeRAWConvert-05.czi': 0.65560144,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-06-ApoTomeRAWConvert-06.czi': 0.86282384,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-07-ApoTomeRAWConvert-07.czi': 0,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-08-ApoTomeRAWConvert-08.czi': 0.78203773,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-09-ApoTomeRAWConvert-09.czi': 0.0002095136,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-10-ApoTomeRAWConvert-10.czi': 0.54944026,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-11-ApoTomeRAWConvert-11.czi': 0.84402895,
+'exp-20210305_sgs3-Gal4_UAS-neg_Pc-Red_Ha-Green_DNA-DAPI-12-ApoTomeRAWConvert-12.czi': 0.8944064,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-01-ApoTomeRAWConvert-13.czi': 0.59632117,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-02-ApoTomeRAWConvert-14.czi': 0.44186217,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-03-ApoTomeRAWConvert-15.czi': 0.57379615,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-04-ApoTomeRAWConvert-16.czi': 0.506644,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-05-ApoTomeRAWConvert-17.czi': 0.60025895,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-06-ApoTomeRAWConvert-18.czi': 0.5812541,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-07-ApoTomeRAWConvert-19.czi': 0.5198922,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-08-ApoTomeRAWConvert-20.czi': 0.57119703,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-09-ApoTomeRAWConvert-21.czi': 0.49534872,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-10-ApoTomeRAWConvert-22.czi': 0.4512847,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-11-ApoTomeRAWConvert-23.czi': 0.35807288,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-12-ApoTomeRAWConvert-24.czi': 0.95481193,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-13-ApoTomeRAWConvert-25.czi': 0.8902668,
+'exp-20210305_sgs3-Gal4_UAS-Psc_Pc-Red_Ha-Green_DNA-DAPI-14-ApoTomeRAWConvert-26.czi': 0.90605366,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-01-ApoTomeRAWConvert-15.czi': 0.5952093,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-02-ApoTomeRAWConvert-16.czi': 0.89625925,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-03-ApoTomeRAWConvert-17.czi': 0.8464197,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-04-ApoTomeRAWConvert-18.czi': 0.7309726,
+'exp-20210305_sgs3-Gal4_UAS-Trr_Pc-Red_Ha-Green_DNA-DAPI-05-ApoTomeRAWConvert-19.czi': 0.7753076,
+}
+dataset_ids = [19, 26, 33, 40, 89, 96, 117, 124, 131, 138, 145, 159, 166, 173, 180, 152]
+
+import omero
+from omero.gateway import BlitzGateway
+from getpass import getpass
+
+
+conn = BlitzGateway(input("Username: "),
+                    getpass("OMERO Password: "),
+                    host=input('Host: '),
+                    port=int(input('Port: ')),
+                    group=input('Group: '),
+                    secure=True)
+conn.connect()
+
+try:
+    for dataset_id in dataset_ids:
+        dataset = conn.getObject('Dataset', dataset_id)
+        images = dataset.listChildren()
+        for image in images:
+            key_value_data = [["manders_HA_Pc", str(manders_HA_Pc[image.getName()])], ["manders_Pc_HA", str(manders_Pc_HA[image.getName()])]]
+            map_ann = omero.gateway.MapAnnotationWrapper(conn)
+            # Use 'client' namespace to allow editing in Insight & web
+            namespace = 'CellProfiler/4/annotated'
+            map_ann.setNs(namespace)
+            map_ann.setValue(key_value_data)
+            map_ann.save()
+            image.linkAnnotation(map_ann)
+
+finally:
+    conn.close()
